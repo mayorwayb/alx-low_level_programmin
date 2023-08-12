a272314a0132feed90379c14660505e63d24c249
mayorwayb <mayorwayb@gmail.com>
2023-08-12 09:29:38 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Enrty point
  *
- * Return: Always o
+ * Return: Always 0
  */
-
 int main(void)
 {
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		printf("%d\n", i);
+		putchar(i + '0');
+	putchar('\n');
 	return (0);
 }
