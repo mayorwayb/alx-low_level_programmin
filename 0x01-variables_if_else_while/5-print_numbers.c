ddd3bcb1c71d14038f14197b8b4cdfe66d17237c
mayorwayb <mayorwayb@gmail.com>
2023-08-12 09:41:42 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: Always o
+ * Return: Always 0
  */
 
 int main(void)
@@ -10,6 +10,7 @@ int main(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		printf("%d\n", i);
+		printf("%d", i);
+	printf("\n");
 	return (0);
 }
