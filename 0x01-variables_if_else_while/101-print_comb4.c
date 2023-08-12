feb4598d2a659d5a7a173871e261f044594a9c20
mayorwayb <mayorwayb@gmail.com>
2023-08-12 14:59:27 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,23 +8,20 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0 ; i < 8 ; i++)
+	for (i = 0 ; i <= 7 ; i++)
 	{
-		for (j = i + 1 ; j <= 9 ; j++)
+		for (j = i + 1 ; j <= 8 ; j++)
 		{
-			for (k = j + 1 ; k <= 10 ; k++)
+			for (k = j + 1 ; k <= 9 ; k++)
 			{
-				if (i != j && i != k && j != k)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
-					if (i + j + k < 24)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
