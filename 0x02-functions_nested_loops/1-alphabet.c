dc174348a1fc7fda1e96502afcdcdd05fd285bf6
mayorwayb <mayorwayb@gmail.com>
2023-08-15 15:23:33 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+		_putchar(a);
+	_putchar('\n');
+}
