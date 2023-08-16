22e27c69b127151984a1244f1a9f4a917da19eb7
mayorwayb <mayorwayb@gmail.com>
2023-08-16 13:25:54 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,17 @@
 int main(void)
 {
 	int n = 98;
-	unsigned long long fib1 = 1, fib2 = 2;
+	long fib1 = 1, fib2 = 2;
 
 	int i;
 
-	printf("%llu, %llu", fib1, fib2);
+	printf("%ld, %ld", fib1, fib2);
 	i = 3;
 	for (; i <= n ; i++)
 	{
-		unsigned long long nextFib = fib1 + fib2;
+		long nextFib = fib1 + fib2;
 
-		printf(", %llu", nextFib);
+		printf(", %ld", nextFib);
 
 		fib1 = fib2;
 		fib2 = nextFib;
