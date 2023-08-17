2894746dbb5f1e1189740b623ead9fcc78719ce8
mayorwayb <mayorwayb@gmail.com>
2023-08-17 12:24:25 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -4,6 +4,8 @@
 /**
 * main - takes a date and prints how many days are left in the year, taking
 * leap years into account
+* @month: parameter
+* @day: parameter
 * Return: 0
 */
 
@@ -18,7 +20,7 @@ int main(void)
 	year = 1997;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-	
+
 	day = convert_day(month, day);
 
 	print_remaining_days(month, day, year);
