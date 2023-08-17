4feafbc6cf72683f884aaf5080581620f8c8a2d0
mayorwayb <mayorwayb@gmail.com>
2023-08-17 13:36:05 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplication function
+ * @a: parameter0
+ * @b: parameter1
+ * Return:a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * Return:
+ */
+void print_numbers(void)
+{
+}
