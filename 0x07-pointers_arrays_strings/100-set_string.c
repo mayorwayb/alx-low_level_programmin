2b84ca3eb0fa9208ba6ed1b8f74e77ba18899ad1
mayorwayb <mayorwayb@gmail.com>
2023-08-29 15:36:44 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - function that set the pointer of a value to a char
+ * @s: parameter
+ * @to: parameter
+ * Return: Void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
