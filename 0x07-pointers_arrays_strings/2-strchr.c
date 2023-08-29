ecff9b04d714b84375d06b004f4287bfa1d6aafc
mayorwayb <mayorwayb@gmail.com>
2023-08-28 17:18:36 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strchr - function that locates a character in a string
+ * @s: parameter
+ * @c: parameter
+ * Return: s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
