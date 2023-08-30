ed01ce755be6fb388d2b24573c962d88067edb07
mayorwayb <mayorwayb@gmail.com>
2023-08-30 16:42:33 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value of x power of y
+ * @x: parameter
+ * @y: parameter
+ * Return: -1, 1 or x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x *  _pow_recursion(x, (y - 1)));
+	return (0);
+}
