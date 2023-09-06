6da5805bf22780363845ac5dbda2eaac69a12c6c
mayorwayb <mayorwayb@gmail.com>
2023-09-05 17:51:02 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * **alloc_grid - function that retuurns pionter to dimen_arr of int
+ * @width: parameter
+ * @height: parameter
+ * Return: NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int **result, a, b;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	result = malloc(sizeof(int *) * height);
+
+	if (result == NULL)
+		return (NULL);
+	for (a = 0 ; a < height ; a++)
+	{
+		result[a] = malloc(sizeof(int *) * width);
+
+		if (result[a] == NULL)
+		{
+			free(result);
+
+			for (b = 0 ; b <= height ; b++)
+				free(result[b]);
+			return (NULL);
+		}
+		for (b = 0 ; b < width ; b++)
+			result[a][b] = 0;
+	}
+	return (result);
+}
