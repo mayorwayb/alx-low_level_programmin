7c536a5deb7448a3e1b0c72a15659f7cb825b236
mayorwayb <mayorwayb@gmail.com>
2023-09-13 17:28:47 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * @array: parameter
+ * @size: parameter
+ * @cmp: parameter
+ * Return: a pointer or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+
+	return (-1);
+}
