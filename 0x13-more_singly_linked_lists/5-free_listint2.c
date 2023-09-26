ee8fe0b1213b33dde3a35ffeae31d7e8f165f473
mayorwayb <mayorwayb@gmail.com>
2023-09-26 12:08:16 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 -  function that frees a listint_t
+ * @head: pointer to the head node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *pointer;
+	listint_t **trans = head;
+
+	if (trans != NULL)
+	{
+		while (*head != NULL)
+		{
+			pointer = *head;
+			free(pointer);
+			*head = (*head)->next;
+		}
+		*trans = NULL;
+	}
+}
